622143559f0bc9dc9a841efb7db9cb6fef4c69e1
LavieMaru <laviemaru@gmail.com>
2016-11-21 16:57:09 -0600

    react homepage.js and app.js error

---
+++ js/components/Homepage.js
@@ -0,0 +1,37 @@
+import React,  {Component} from 'react';
+import logo from '../../src/logo.svg';
+
+const Homepage = () => {
+  return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to whatSUP Messenger</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+};
+
+export default Homepage;
+
+
+/*class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to whatSUP Messenger</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;*/
--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from '../js/store';
+import {Router, Route, hashHistory} from 'react-router';
+
+//import components
+import Homepage from '../js/components/Homepage'
+
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React, Lavie & Matt!</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+console.log('Client running in ${process.env.NODE_ENV} mode');
 
-export default App;
+//Routes
+const routes = (
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={Homepage}/>
+    </Router>
+  </Provider>
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(routes, document.getElementId('app'));
+});
